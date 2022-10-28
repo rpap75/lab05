602b7491e303b1abcf4d80532b5e046238af9775
Ryan Papsin <ryanpapsin175@gmail.com>
2022-10-28 14:54:12 -0700

    lab5q3

--- js/app.js
+++ js/app.js
@@ -28,11 +28,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  let multiply = a * b;
+  return [multiply, `The product of ${a} and ${b} is ${multiply}.`];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -49,11 +50,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  let firstSum = sum(a, b)[0];
+  console.log(firstSum);
+  let finalSum = sum(firstSum, c)[0];
+  let firstProduct = multiply(a, b)[0];
+  let finalProduct = multiply (firstProduct, c)[0];
+  return [finalSum, finalProduct, `${a} and ${b} and ${c} sum to ${finalSum}.`, `The product of ${a} and ${b} and ${c} is ${finalProduct}.`];
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
